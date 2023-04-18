8babee1b410eae8515eb5474cffe85ac98c8d9fd
Neel <neelpatel3579@gmail.com>
2023-04-18 09:46:03 +0530

    100%

--- php/signup.php
+++ php/signup.php
@@ -19,47 +19,32 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Prepare SQL statement
-$stmt = $conn->prepare("INSERT INTO userdata (name, username, email, password) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("ssss", $name, $username, $email, $password);
+// Check if user already exists
+$check_stmt = $conn->prepare("SELECT username FROM userdata WHERE username = ?");
+$check_stmt->bind_param("s", $username);
 
-// Execute query
-if ($stmt->execute()) {
-    function sendEmail($to, $from, $fromName, $subject, $message) {
-        // Set additional headers
-        $headers = "From: $fromName <$from>\r\n";
-        $headers .= "Reply-To: $from\r\n";
-        $headers .= "Content-type: text/html\r\n";
-      
-        // Send the email using the mail() function
-        if (mail($to, $subject, $message, $headers)) {
-            echo "Email sent successfully.";
-        } else {
-            echo "Email sending failed.";
-        }
-      }
+$check_stmt->execute();
+$check_stmt->store_result();
+
+if ($check_stmt->num_rows > 0) {
+    // User already exists, display error message or redirect to an error page
+    header("Location: ../signup.html?error=Username is already exist");
+} else {
+    // User does not exist, insert data into the database
+    $stmt = $conn->prepare("INSERT INTO userdata (name, username, email, password) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $name, $username, $email, $password);
+
+    if ($stmt->execute()) {
+        // Send confirmation email and redirect to homepage
+            
+
+        header("Location: ../homepage.html");
+    }
     
-    $to = $email;
-$from = "certifyup.community@gmail.com";
-$fromName = "CertifyUp";
-$subject = "Account Creation Confirmation";
-$message = "Dear $name,
+    $stmt->close();
+}
 
-We would like to take this opportunity to welcome you to [website name], the certificate generator website that offers a range of services to help you create and manage your certificates.
-
-We are happy to inform you that your account has been successfully created. You can now log in to your account and access all the features available on our website.
-
-If you have any questions, please do not hesitate to contact us. We are always here to help you and provide you with the best possible service.
-
-Thank you for choosing [website name]. We hope you enjoy using our platform and look forward to seeing the certificates you create.
-
-Best regards,
-[Your Name]";      
-sendEmail($to, $from, $fromName, $subject, $message);
-header("Location: ../homepage.html");}
-// Close statement and connection
-$stmt->close();
+// Close check statement and database connection
+$check_stmt->close();
 $conn->close();
-
-
 ?>
